f1aa32240ee9855f664bce1d78eb2f3e10d324ab
Mateusz Matusiak <mateusz.matusiak@likims.com>
2022-10-10 14:26:14 +0200

    Initial commit

---
+++ src/main/java/com/example/dangerousplace/DangerousPlaceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dangerousplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangerousPlaceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangerousPlaceAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dangerousplace/DangerousPlaceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dangerousplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangerousPlaceAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
